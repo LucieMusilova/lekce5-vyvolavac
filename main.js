994623fbc905cf11c4878673cedae64e126da8a9
Lucie Musilová <musil.lucie@seznam.cz>
2021-04-13 20:47:45 +0200

    done

--- main.js
+++ main.js
@@ -1,45 +1,78 @@
+
 let jmena = [
-    "Markéta",
-    "Kateřina",
-    "Júlia",
-    "Lucie",
-    "Aneta",
-    "Veronika",
-    "Petra",
-    "Olga",
-    "Naďa",
-    "Nikola",
-    "Eva",
-    "Anísa",
-    "Terezia",
-    "Diana",
-    "Anna",
-    "Iva",
-    "Denisa",
-    "Zuzana",
-    "Jana",
-    "Martina",
-    "Lenka",
-    "Iveta",
-    "Pavla",
-    "Lucie",
-    "Kamila",
-    "Soňa"
+  "Markéta",
+  "Kateřina",
+  "Júlia",
+  "Lucie",
+  "Aneta",
+  "Veronika",
+  "Petra",
+  "Olga",
+  "Naďa",
+  "Nikola",
+  "Eva",
+  "Anísa",
+  "Terezia",
+  "Diana",
+  "Anna",
+  "Iva",
+  "Denisa",
+  "Zuzana",
+  "Jana",
+  "Martina",
+  "Lenka",
+  "Iveta",
+  "Pavla",
+  "Lucie",
+  "Kamila",
+  "Soňa"
 ];
+
 
 let tazenaJmena = [];
 
 function tahnoutJmeno() {
-    if (jmena.length === 0) {
-        return;
-    }
+  if (jmena.length === 0) {
+      return;
+  }
 
-    // Generujeme náhodný index
+  // Generujeme náhodný index
 
-    // Získáme výherní jméno na patřičném indexu
+  let vyherniIndex = Math.floor(Math.random() * jmena.length);
+  console.log(vyherniIndex);
 
-    // Vyřadíme vylosované jméno z osudí
+  // Získáme výherní jméno na patřičném indexu
 
-    // Výherní jméno si uložíme do pole k ostatním výherním
+  let vyherniJmeno = jmena[vyherniIndex];
+  console.log(vyherniJmeno);
+
+  // Vyřadíme vylosované jméno z osudí
+
+  jmena.splice(vyherniIndex, 1);
+  console.log(jmena.length);
+
+  // Výherní jméno si uložíme do pole k ostatním výherním
+
+  tazenaJmena.unshift(vyherniJmeno);
+
+  // Počet výherkyň
+  
+  let pocet = document.querySelector("#pocet");
+  pocet.innerHTML = parseInt(pocet.innerHTML) + 1;
     
+  
+  
+
+
+  let vyherce = document.querySelector("#vyherka");
+  vyherce.innerHTML = vyherniJmeno;
+
+  let seznamJmen = document.querySelector("#seznam");
+  seznamJmen.innerHTML = tazenaJmena;
+}
+
+// reset hry
+
+function reset() {
+  location.reload();
 }
